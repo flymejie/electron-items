93a11c0874956cacbad01dd215d7d4b86ee27fb2
flymejie <flymejie@163.com>
2020-08-21 17:55:14 +0800

    '20200821/1755'

--- public/javascripts/show/action.js
+++ public/javascripts/show/action.js
@@ -1,12 +1,38 @@
+
+(function(){
+    window.sessionStorage.setItem('acceptHttp','192.168.0.124:3300');
+})();
+
+var acceptSocket = io.connect('192.168.0.124:3300');
+
+
+
 /* 点击事件传参 切换中英文 */
 let langNum = 0; /// 0 china  1 english;
 let sendSocket = new socketIo();
+
+acceptSocket.on('sendChangeLang',function(data){
+    langNum = data.lang;
+    console.log('sendChangeLang',data,langNum);
+})
+
+acceptSocket.on('sendRouterMsg',function(data){
+    console.log('sendRouterMsg',data);
+    sendSocket.sendChangeLang(langNum);
+    window.location.href = '/users' + data.router;
+})
+
+
+
+
+
 
 $('.vide_lan').on('click','img',function () {
     $.each($('.vide_lan').children('img'), (n, m) => {
         $(m).attr('src', $(m).data('src'));
     });
 });
+
 
 $('.vide_list').on('click', 'li img', function () {
     let that = $(this);
@@ -26,6 +52,7 @@ $('.vide_list').on('click', 'li img', function () {
     }
 
 });
+
 
 
 
